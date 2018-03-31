48aa72b60d97331862bef92a0dc3534a0bc4b494
eep <coolicer@foxmail.com>
2018-03-31 12:53:50 +0800

    add TODO

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ App({
     // var logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
-
+    let self = this;
     // 登录
     wx.checkSession({
       fail: function() {
@@ -19,6 +19,9 @@ App({
               let code = res.code;
               wx.getUserInfo({
                 success: (res) => {
+                  self.globalData.userInfo = res.userInfo;
+                  wx.setStorageSync('userInfo', res.userInfo);
+                  // 用于注册用户
                   wx.request({
                     url: '', // 要使用https的api接口
                     method: 'post',
@@ -30,7 +33,7 @@ App({
                       iv: res.iv, 
                       code: code
                     },
-                    success: _getUserInfo
+                    success: _loginToken
                   })
                 }
               });
@@ -41,10 +44,21 @@ App({
         })
       }
     });
-
-    // 解密成功后 获取自己服务器返回的结果
-    function _getUserInfo(res) {
-      
+    function _loginToken(token) {
+      // 返回token，下次用户再去执行一些需要验证身份的动作的时候，要带上一个 Authorization 这个 Header，对应的值是 Bearer 空格后面再加上具体的 Token 的值。
+      // wx.request({
+      //   url: `${ API_BASE }/${ API_ROUTE }`,
+      //   method: 'POST',
+      //   header: {
+      //     'Authorization': `Bearer ${ token }`
+      //   },
+      //   data: {
+      //     ...
+      //   },
+      //   success: (res) => {
+       
+      //   }
+      // })
     }
   },
   globalData: {
