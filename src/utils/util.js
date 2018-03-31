4c96410b653f7c6a04d7b148dcc01ec29e0b206d
eep <coolicer@foxmail.com>
2018-03-31 11:58:10 +0800

    login

--- src/utils/util.js
+++ src/utils/util.js
@@ -14,6 +14,11 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const getAuthorize = function(wx, scope) {
+
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getAuthorize: getAuthorize
 }
